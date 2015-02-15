5f5f2b75fa3d5a99c6893c373fd399ce0ac353ff
tetsuo <ogunduz@gmail.com>
2015-02-15 13:02:37 +0200

    do not return nodes

--- index.js
+++ index.js
@@ -10,35 +10,40 @@ module.exports = function (arr) {
   var edges = [], index = {};
   arr.forEach(function (obj) {
     index[obj.name] = obj;
-    [].concat(obj.deps).filter(Boolean).forEach(function (name) {
+    [].concat(obj.locals).filter(Boolean).forEach(function (name) {
       edges.push([obj.name, name]);
     });
   });
 
   nodes = toposort.array(nodes, edges).map(function (name) {
     var obj = index[name];
-    return xtend(index[name], { _deps: obj.deps || [] });
+    return xtend(index[name], { _locals: obj.locals || [] });
   });
 
   var groups = nodes.reduce(function (acc, dependency) {
     var dependents = acc.filter(function (obj) {
-      if (~obj._deps.indexOf(dependency.name))
+      if (~obj._locals.indexOf(dependency.name))
         return obj;
     });
     
     if (dependents.length == 1) {
       var dependent = dependents[0];
-      var dups = intersect(dependent._deps, dependency._deps);
-      dependent._deps = dependent._deps.filter(function (name) {
+      var dups = intersect(dependent._locals, dependency._locals);
+      dependent._locals = dependent._locals.filter(function (name) {
         if (~dups.indexOf(name)) return;
         return true;
       });
-      dependent._deps = dependent._deps.concat(dependency._deps);
+      dependent._locals = dependent._locals.concat(dependency._locals);
       return acc;
     }
 
     return acc.concat(dependency);
   }, []);
 
-  return { groups: groups, nodes: nodes };
+  return groups.map(function (group) {
+    return {
+      name: group.name,
+      locals: group._locals
+    };
+  });
 }
--- test/groups.js
+++ test/groups.js
@@ -3,44 +3,44 @@ var test = require('tape');
 
 test('groups', function (t) {
   var groups = cluster([
-    { name: 'x', deps: ['y', 'j'] },
+    { name: 'x', locals: ['y', 'j'] },
     { name: 'j' },
-    { name: 'y', deps: ['j'] }
-  ]).groups;
-  
+    { name: 'y', locals: ['j'] }
+  ]);
+
   t.equal(groups.length, 1);
   t.equal(groups[0].name, 'x');
-  t.deepEqual(groups[0]._deps, ['y', 'j']);
+  t.deepEqual(groups[0].locals, ['y', 'j']);
 
   groups = cluster([
-    { name: 'x', deps: ['y', 'j'] },
+    { name: 'x', locals: ['y', 'j'] },
     { name: 'j' },
-    { name: 'y', deps: ['j'] },
-    { name: 'k', deps: ['j'] }
-  ]).groups;
+    { name: 'y', locals: ['j'] },
+    { name: 'k', locals: ['j'] }
+  ]);
 
   t.equal(groups.length, 3);
   t.equal(groups[0].name, 'x');
   t.equal(groups[2].name, 'j');
   t.equal(groups[1].name, 'k');
-  t.deepEqual(groups[1]._deps, ['j']);
+  t.deepEqual(groups[1].locals, ['j']);
 
   groups = cluster([
-    { name: 'x', deps: ['y', 'j'] },
-    { name: 'y', deps: ['j'] },
-    { name: 'k', deps: ['j', 'a'] },
-    { name: 'j', deps: ['a'] },
+    { name: 'x', locals: ['y', 'j'] },
+    { name: 'y', locals: ['j'] },
+    { name: 'k', locals: ['j', 'a'] },
+    { name: 'j', locals: ['a'] },
     { name: 'w' },
-    { name: 'a', deps: ['w'] }
-  ]).groups;
+    { name: 'a', locals: ['w'] }
+  ]);
 
   t.equal(groups.length, 4);
   t.equal(groups[3].name, 'a');
   t.equal(groups[0].name, 'x');
   t.equal(groups[1].name, 'k');
   t.equal(groups[2].name, 'j');
-  t.deepEqual(groups[2]._deps, ['a']);
-  t.deepEqual(groups[3]._deps, ['w']);
+  t.deepEqual(groups[2].locals, ['a']);
+  t.deepEqual(groups[3].locals, ['w']);
 
   t.end();
 });
